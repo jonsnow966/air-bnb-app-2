e80f79ebebec7bd05e3efa292437eb6a8bbd94a1
jonsnow966 <jonsnow966usk@gmail.com>
2025-06-02 13:02:25 +0500

    3rd commit - Responsiveness - 02/06/2025

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,9 @@ export default {
       screens: {
         'xs': '500px',
         'sm': '770px',
-        'md': '850px',
+        'md': '870px',
         'lg': '992px',
         'xl': '1200px',
-        // '2xl':'1280',
       },
     },
   },
