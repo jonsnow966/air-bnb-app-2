5e097d08b5c97c086cbe9ea7e3b2fadf68eb7c1c
jonsnow966 <jonsnow966usk@gmail.com>
2025-06-02 13:19:11 +0500

    5th commit - Responsiveness - 02/06/2025

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],  theme: {
     screens: {
-        'xs': '500px',
-        'sm': '770px',
-        'md': '870px',
-        'lg': '992px',
-        'xl': '1200px',
-      },
+        xs: '500px',
+        sm: '770px',
+        md: '870px',
+        lg: '992px',
+        xl: '1200px',
+    },
     extend: {
     },
   },
